586bf960870c220d813013083e3efd7fc02e4af7
lucky1975 <ucha@justsomecoding.com>
2023-01-06 11:27:10 +0100

    fx

--- server/setup.js
+++ server/setup.js
@@ -1,7 +1,6 @@
+require('fs');
 require('path');
 require('dotenv').config({path:path.resolve(__dirname+'/.env')});
 require('ignore-styles');
-require('@babel/register')({
-    ignore: [/(node_module)/], presets: ['@babel/preset-env', '@babel/preset-react']
-});
+require('@babel/register')({ignore: [/(node_module)/], presets: ['@babel/preset-env', '@babel/preset-react']});
 require('./server');
